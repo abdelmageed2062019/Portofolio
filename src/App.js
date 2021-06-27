d570d829b6ff2c3f106111560d878fc652b3133a
abdelmaged2062019 <abdelmaged1@gmail.com>
2021-06-27 17:13:13 +0200

    Navbar completed

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
 import './App.css';
 import "bootstrap/dist/css/bootstrap.min.css" 
+import Navbar from "./components/Navbar"
 function App() {
   return (
-    <h1>New Portofolio</h1>
+    <Navbar />
   );
 }
 
